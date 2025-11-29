5589fd7a7287e59bd6d6f911c4387d63111c174a
[dyad] <ezratatsumi@hotmail.com>
2025-11-29 03:05:35 -0300

    [dyad] Otimizando o contexto do código para liberar espaço de tokens. - wrote 1 file(s), deleted 64 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,19 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "next-themes";
-import { SessionContextProvider } from "./components/SessionContextProvider"; // Importar SessionContextProvider
-import Index from "./pages/Index";
+import { SessionContextProvider } from "./components/SessionContextProvider";
 import Login from "./pages/Login";
 import PTrabManager from "./pages/PTrabManager";
 import PTrabForm from "./pages/PTrabForm";
 import PTrabPrint from "./pages/PTrabPrint";
 import ClasseIForm from "./pages/ClasseIForm";
-import ClasseIIForm from "./pages/ClasseIIForm"; // Importar ClasseIIForm
+import ClasseIIForm from "./pages/ClasseIIForm";
 import ClasseIIIForm from "./pages/ClasseIIIForm";
 import DiretrizesCusteioPage from "./pages/DiretrizesCusteioPage";
 import VisualizacaoConfigPage from "./pages/VisualizacaoConfigPage";
 import OmConfigPage from "./pages/OmConfigPage";
 import OmBulkUploadPage from "./pages/OmBulkUploadPage";
 import PTrabExportImportPage from "./pages/PTrabExportImportPage";
-import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
@@ -29,22 +27,24 @@ const App = () => (
         <Toaster />
         <Sonner />
         <BrowserRouter>
-          <SessionContextProvider> {/* Envolver a aplicação com SessionContextProvider */}
+          <SessionContextProvider>
             <Routes>
-              <Route path="/" element={<Index />} />
+              {/* Rota raiz agora aponta para o gerenciador de PTrab (que redireciona para login se deslogado) */}
+              <Route path="/" element={<PTrabManager />} /> 
               <Route path="/login" element={<Login />} />
               <Route path="/ptrab" element={<PTrabManager />} />
               <Route path="/ptrab/form" element={<PTrabForm />} />
               <Route path="/ptrab/print" element={<PTrabPrint />} />
               <Route path="/ptrab/classe-i" element={<ClasseIForm />} />
-              <Route path="/ptrab/classe-ii" element={<ClasseIIForm />} /> {/* Nova Rota */}
+              <Route path="/ptrab/classe-ii" element={<ClasseIIForm />} />
               <Route path="/ptrab/classe-iii" element={<ClasseIIIForm />} />
               <Route path="/config/diretrizes" element={<DiretrizesCusteioPage />} />
               <Route path="/config/visualizacao" element={<VisualizacaoConfigPage />} />
               <Route path="/config/om" element={<OmConfigPage />} />
               <Route path="/config/om/bulk-upload" element={<OmBulkUploadPage />} />
               <Route path="/config/ptrab-export-import" element={<PTrabExportImportPage />} />
-              <Route path="*" element={<NotFound />} />
+              {/* Removida a rota 404 e a rota Index */}
+              <Route path="*" element={<PTrabManager />} />
             </Routes>
           </SessionContextProvider>
         </BrowserRouter>
