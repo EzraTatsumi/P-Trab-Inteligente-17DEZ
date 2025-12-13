8d51cf3e6a9443c86852da7cea1ebd068f05e1c1
[dyad] <ezratatsumi@hotmail.com>
2025-12-13 18:54:09 -0300

    [dyad] wrote 6 file(s) + extra files edited outside of Dyad

---
+++ src/data/classeVData.ts
@@ -0,0 +1,9 @@
+import { DiretrizClasseIIForm } from "@/types/diretrizesClasseII";
+
+export const defaultClasseVConfig: DiretrizClasseIIForm[] = [
+  { categoria: "Armt L", item: "Fuzil 5,56mm", valor_mnt_dia: 0.50 },
+  { categoria: "Armt L", item: "Pistola 9mm", valor_mnt_dia: 0.30 },
+  { categoria: "Armt P", item: "Metralhadora .50", valor_mnt_dia: 5.00 },
+  { categoria: "IODCT", item: "Kit IODCT Básico", valor_mnt_dia: 1.50 },
+  { categoria: "DQBRN", item: "Kit DQBRN Individual", valor_mnt_dia: 0.80 },
+];
