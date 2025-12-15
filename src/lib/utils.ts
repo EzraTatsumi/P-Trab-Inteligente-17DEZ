19f08709e93ad34c4f876784f98f27f33eb730aa
[dyad] <ezratatsumi@hotmail.com>
2025-12-15 15:16:39 -0300

    Reverted all changes back to version e5b498b01ecfded7260d8d686293fa7914803002

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,13 +1,6 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
-
-export function formatCurrency(amount: number): string {
-  return new Intl.NumberFormat('pt-BR', {
-    style: 'currency',
-    currency: 'BRL',
-  }).format(amount);
+  return twMerge(clsx(inputs));
 }
