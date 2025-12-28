ef51677785fc2629002c1576dd41e63276f7d89a
[dyad] <ezratatsumi@hotmail.com>
2025-12-28 12:52:39 +0200

    [dyad] Forçando o rastreamento interno de DiretrizesCusteioPage.tsx - wrote 1 file(s)

--- src/pages/DiretrizesCusteioPage.tsx
+++ src/pages/DiretrizesCusteioPage.tsx
@@ -96,9 +96,9 @@ const CATEGORIAS_CLASSE_V = [
 ];
 
 const CATEGORIAS_CLASSE_VI = [
+  "Gerador",
   "Embarcação",
   "Equipamento de Engenharia",
-  "Gerador", // Mantido 'Gerador' aqui
 ];
 
 const CATEGORIAS_CLASSE_VII = [
@@ -400,7 +400,7 @@ const DiretrizesCusteioPage = () => {
       const { data: classeIXData, error: classeIXError } = await supabase
         .from("diretrizes_classe_ix")
         .select("categoria, item, valor_mnt_dia, valor_acionamento_mensal")
-        .eq("user_id", user.id)
+        .eq("user.id", user.id)
         .eq("ano_referencia", year)
         .eq("ativo", true);
         
