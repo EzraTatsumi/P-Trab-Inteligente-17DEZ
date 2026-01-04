8feef53ba83347e94678ec794d11006a79c00691
[dyad] <ezratatsumi@hotmail.com>
2026-01-04 14:47:24 +0200

    [dyad] Adicionando a rota /ptrab/diaria ao App.tsx para resolver o erro 404 - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -27,6 +27,7 @@ import PTrabExportImportPage from "./pages/PTrabExportImportPage";
 import UserProfilePage from "./pages/UserProfilePage";
 import SharePage from "./pages/SharePage"; // Importar SharePage
 import ResetPasswordPage from "./pages/ResetPasswordPage"; // Importar ResetPasswordPage
+import DiariaForm from "./pages/DiariaForm"; // Importar DiariaForm
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -54,6 +55,7 @@ const App = () => (
               <Route path="/ptrab/classe-viii" element={<ClasseVIIIForm />} />
               <Route path="/ptrab/classe-ix" element={<ClasseIXForm />} />
               <Route path="/ptrab/classe-iii" element={<ClasseIIIForm />} />
+              <Route path="/ptrab/diaria" element={<DiariaForm />} /> {/* ROTA ADICIONADA */}
               <Route path="/config/custos-operacionais" element={<CustosOperacionaisPage />} />
               <Route path="/config/diretrizes" element={<DiretrizesCusteioPage />} />
               <Route path="/config/visualizacao" element={<VisualizacaoConfigPage />} />
