f01c4798e74af7a8a6e86b06ce6fe0a8ec2644a1
[dyad] <ezratatsumi@hotmail.com>
2026-01-29 16:22:07 +0200

    Reverted all changes back to version dad82f0e8e31c1f81fdd84a21454dc1169382666

--- src/components/CurrencyInput.tsx
+++ src/components/CurrencyInput.tsx
@@ -1,47 +1,53 @@
-import React from 'react';
-import { Input } from "@/components/ui/input";
+import React, { useMemo, useCallback } from 'react';
+import { Input, InputProps } from "@/components/ui/input";
 import { formatCurrencyInput } from "@/lib/formatUtils";
-import { useFormNavigation } from "@/hooks/useFormNavigation";
+import { cn } from "@/lib/utils";
 
-interface CurrencyInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
-  /** A string de dígitos brutos (ex: "123456" para R$ 1.234,56). */
+interface CurrencyInputProps extends Omit<InputProps, 'value' | 'onChange' | 'onBlur'> {
   rawDigits: string;
-  /** Callback chamado com a nova string de dígitos brutos. */
   onChange: (digits: string) => void;
+  placeholder?: string;
+  className?: string;
 }
 
-const CurrencyInput = React.forwardRef<HTMLInputElement, CurrencyInputProps>(
-  ({ rawDigits, onChange, onKeyDown, ...props }, ref) => {
-    const { formatted } = formatCurrencyInput(rawDigits);
-    const { handleEnterToNextField } = useFormNavigation();
-
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-      // Remove tudo que não for dígito
-      const newDigits = e.target.value.replace(/\D/g, '');
-      onChange(newDigits);
-    };
+export const CurrencyInput = React.forwardRef<HTMLInputElement, CurrencyInputProps>(
+  ({ rawDigits, onChange, placeholder = "0,00", className, ...props }, ref) => {
     
-    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-        if (e.key === 'Enter') {
-            handleEnterToNextField(e);
-        }
-        if (onKeyDown) {
-            onKeyDown(e);
-        }
-    };
+    // Calcula o valor formatado com base na prop rawDigits
+    const { formatted } = useMemo(() => formatCurrencyInput(rawDigits), [rawDigits]);
+
+    const handleInputChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+      const rawValue = e.target.value;
+      
+      // 1. Remove tudo que não for dígito
+      const newDigits = rawValue.replace(/\D/g, '');
+      
+      // 2. Notifica o componente pai com os novos dígitos brutos
+      onChange(newDigits);
+    }, [onChange]);
+
+    const handleInputBlur = useCallback(() => {
+      // Se o input estiver vazio, garante que o estado pai receba uma string vazia
+      if (rawDigits.length === 0) {
+          onChange('');
+      }
+    }, [rawDigits, onChange]);
 
     return (
-      <Input
-        ref={ref}
-        type="text"
-        inputMode="numeric"
-        // Exibe o valor formatado, mas usa o rawDigits para controle
-        value={formatted}
-        onChange={handleChange}
-        onKeyDown={handleKeyDown}
-        className="[appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
-        {...props}
-      />
+      <div className="relative">
+        <Input
+          ref={ref}
+          type="text"
+          inputMode="decimal"
+          value={formatted} // Usa a string formatada para exibição
+          onChange={handleInputChange}
+          onBlur={handleInputBlur}
+          placeholder={placeholder}
+          className={cn("pl-12 text-lg", className)}
+          {...props}
+        />
+        <span className="absolute left-2 top-1/2 -translate-y-1/2 text-lg text-foreground">R$</span>
+      </div>
     );
   }
 );
