fd4b98f8ccad68f6c2decbb2a8760265cacbc53d
[dyad] <ezratatsumi@hotmail.com>
2026-01-31 00:01:44 +0200

    [dyad] Desabilitando a alteração do valor do input de quantidade de passagens pelas setas do teclado. - wrote 1 file(s)

--- src/pages/PassagemForm.tsx
+++ src/pages/PassagemForm.tsx
@@ -1278,6 +1278,12 @@ const PassagemForm = () => {
                                                                                         placeholder="Ex: 3"
                                                                                         value={trecho.quantidade_passagens === 0 ? "" : trecho.quantidade_passagens}
                                                                                         onChange={(e) => handleTrechoQuantityChange(trecho.id, parseInt(e.target.value) || 0)}
+                                                                                        onWheel={(e) => e.currentTarget.blur()} // Desabilita roda do mouse
+                                                                                        onKeyDown={(e) => { // Desabilita setas do teclado
+                                                                                            if (e.key === 'ArrowUp' || e.key === 'ArrowDown') {
+                                                                                                e.preventDefault();
+                                                                                            }
+                                                                                        }}
                                                                                         className="w-20 text-center h-8 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
                                                                                         disabled={!isPTrabEditable || isSaving}
                                                                                     />
