9a05958b56f666537865c1d4673f7b88a58edbb2
[dyad] <ezratatsumi@hotmail.com>
2026-01-31 19:02:19 +0200

    [dyad] Adicionando PTrabProvider no App.tsx para resolver erro de contexto - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "next-themes";
 import { SessionContextProvider } from "./components/SessionContextProvider";
+import { PTrabProvider } from "./context/PTrabContext"; // Importar PTrabProvider
 import Index from "./pages/Index";
 import Login from "./pages/Login";
 import PTrabManager from "./pages/PTrabManager";
@@ -25,12 +26,12 @@ import OmConfigPage from "./pages/OmConfigPage";
 import OmBulkUploadPage from "./pages/OmBulkUploadPage";
 import PTrabExportImportPage from "./pages/PTrabExportImportPage";
 import UserProfilePage from "./pages/UserProfilePage";
-import SharePage from "./pages/SharePage"; // Importar SharePage
-import ResetPasswordPage from "./pages/ResetPasswordPage"; // Importar ResetPasswordPage
-import DiariaForm from "./pages/DiariaForm"; // Importar DiariaForm
-import VerbaOperacionalForm from "./pages/VerbaOperacionalForm"; // NOVO IMPORT
-import SuprimentoFundosForm from "./pages/SuprimentoFundosForm"; // NOVO IMPORT
-import PassagemForm from "./pages/PassagemForm"; // NOVO IMPORT
+import SharePage from "./pages/SharePage";
+import ResetPasswordPage from "./pages/ResetPasswordPage";
+import DiariaForm from "./pages/DiariaForm";
+import VerbaOperacionalForm from "./pages/VerbaOperacionalForm";
+import SuprimentoFundosForm from "./pages/SuprimentoFundosForm";
+import PassagemForm from "./pages/PassagemForm";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -43,35 +44,37 @@ const App = () => (
         <Sonner />
         <BrowserRouter>
           <SessionContextProvider>
-            <Routes>
-              <Route path="/" element={<Index />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/reset-password" element={<ResetPasswordPage />} /> {/* NOVA ROTA */}
-              <Route path="/ptrab" element={<PTrabManager />} />
-              <Route path="/ptrab/form" element={<PTrabForm />} />
-              <Route path="/ptrab/print" element={<PTrabReportManager />} />
-              <Route path="/ptrab/classe-i" element={<ClasseIForm />} />
-              <Route path="/ptrab/classe-ii" element={<ClasseIIForm />} />
-              <Route path="/ptrab/classe-v" element={<ClasseVForm />} />
-              <Route path="/ptrab/classe-vi" element={<ClasseVIForm />} />
-              <Route path="/ptrab/classe-vii" element={<ClasseVIIForm />} />
-              <Route path="/ptrab/classe-viii" element={<ClasseVIIIForm />} />
-              <Route path="/ptrab/classe-ix" element={<ClasseIXForm />} />
-              <Route path="/ptrab/classe-iii" element={<ClasseIIIForm />} />
-              <Route path="/ptrab/diaria" element={<DiariaForm />} /> {/* ROTA ADICIONADA */}
-              <Route path="/ptrab/verba-operacional" element={<VerbaOperacionalForm />} /> {/* NOVA ROTA */}
-              <Route path="/ptrab/suprimento-fundos" element={<SuprimentoFundosForm />} /> {/* NOVA ROTA */}
-              <Route path="/ptrab/passagem-aerea" element={<PassagemForm />} /> {/* NOVA ROTA */}
-              <Route path="/config/custos-operacionais" element={<CustosOperacionaisPage />} />
-              <Route path="/config/diretrizes" element={<DiretrizesCusteioPage />} />
-              <Route path="/config/visualizacao" element={<VisualizacaoConfigPage />} />
-              <Route path="/config/om" element={<OmConfigPage />} />
-              <Route path="/config/om/bulk-upload" element={<OmBulkUploadPage />} />
-              <Route path="/config/ptrab-export-import" element={<PTrabExportImportPage />} />
-              <Route path="/config/profile" element={<UserProfilePage />} />
-              <Route path="/share-ptrab" element={<SharePage />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
+            <PTrabProvider> {/* Envolvendo as rotas com PTrabProvider */}
+              <Routes>
+                <Route path="/" element={<Index />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/reset-password" element={<ResetPasswordPage />} />
+                <Route path="/ptrab" element={<PTrabManager />} />
+                <Route path="/ptrab/form" element={<PTrabForm />} />
+                <Route path="/ptrab/print" element={<PTrabReportManager />} />
+                <Route path="/ptrab/classe-i" element={<ClasseIForm />} />
+                <Route path="/ptrab/classe-ii" element={<ClasseIIForm />} />
+                <Route path="/ptrab/classe-v" element={<ClasseVForm />} />
+                <Route path="/ptrab/classe-vi" element={<ClasseVIForm />} />
+                <Route path="/ptrab/classe-vii" element={<ClasseVIIForm />} />
+                <Route path="/ptrab/classe-viii" element={<ClasseVIIIForm />} />
+                <Route path="/ptrab/classe-ix" element={<ClasseIXForm />} />
+                <Route path="/ptrab/classe-iii" element={<ClasseIIIForm />} />
+                <Route path="/ptrab/diaria" element={<DiariaForm />} />
+                <Route path="/ptrab/verba-operacional" element={<VerbaOperacionalForm />} />
+                <Route path="/ptrab/suprimento-fundos" element={<SuprimentoFundosForm />} />
+                <Route path="/ptrab/passagem-aerea" element={<PassagemForm />} />
+                <Route path="/config/custos-operacionais" element={<CustosOperacionaisPage />} />
+                <Route path="/config/diretrizes" element={<DiretrizesCusteioPage />} />
+                <Route path="/config/visualizacao" element={<VisualizacaoConfigPage />} />
+                <Route path="/config/om" element={<OmConfigPage />} />
+                <Route path="/config/om/bulk-upload" element={<OmBulkUploadPage />} />
+                <Route path="/config/ptrab-export-import" element={<PTrabExportImportPage />} />
+                <Route path="/config/profile" element={<UserProfilePage />} />
+                <Route path="/share-ptrab" element={<SharePage />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </PTrabProvider>
           </SessionContextProvider>
         </BrowserRouter>
       </TooltipProvider>
