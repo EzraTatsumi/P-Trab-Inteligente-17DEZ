43c652253efa5ef81181830da4d78f938795053e
[dyad] <ezratatsumi@hotmail.com>
2026-02-01 20:52:52 +0200

    Updated src/pages/CustosOperacionaisPage.tsx

--- src/pages/CustosOperacionaisPage.tsx
+++ src/pages/CustosOperacionaisPage.tsx
@@ -857,7 +857,7 @@ const CustosOperacionaisPage = () => {
     const placeholderNome = isAgua ? 'Ex: COSANPA / Saneago' : 'Ex: Equatorial / Enel';
     const placeholderConsumo = isAgua ? 'Ex: 0,15' : 'Ex: 1,5';
     const placeholderCusto = isAgua ? '5,00' : '0,80';
-    const placeholderFonteConsumo = 'Ex: SNIS/2023';
+    const placeholderFonteConsumo = 'Ex: Sistema Nacional de Informação sobre Saneamento - SNIS/2023';
     const placeholderFonteCusto = 'Ex: Tabela de Tarifa Ago/2024';
     
     const getCustoUnitarioProps = (item: DiretrizConcessionariaForm, indexInMainArray: number) => {
