f27307e8bea357e292ce0670e1bee8669f827ff2
[dyad] <ezratatsumi@hotmail.com>
2026-02-05 14:54:20 +0200

    [dyad] Remover tabelas de material de consumo não utilizadas

---
+++ supabase/migrations/0011_drop_unused_database_tables_from_a_previous_failed_component_implementation_.sql
@@ -0,0 +1,4 @@
+DROP TABLE IF EXISTS diretrizes_material_consumo_categorias CASCADE;
+DROP TABLE IF EXISTS diretrizes_material_consumo_itens CASCADE;
+DROP TABLE IF EXISTS material_consumo_itens CASCADE;
+DROP TABLE IF EXISTS material_consumo_subitens CASCADE;
