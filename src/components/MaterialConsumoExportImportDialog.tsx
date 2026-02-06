037f41e72f061b078c8c8282e8db48ab5506c7e1
[dyad] <ezratatsumi@hotmail.com>
2026-02-06 17:11:59 +0200

    Updated src/components/MaterialConsumoExportImportDialog.tsx

--- src/components/MaterialConsumoExportImportDialog.tsx
+++ src/components/MaterialConsumoExportImportDialog.tsx
@@ -325,7 +325,7 @@ const MaterialConsumoExportImportDialog: React.FC<MaterialConsumoExportImportDia
                     {isProcessing ? (
                         <Loader2 className="mr-2 h-4 w-4 animate-spin" />
                     ) : (
-                        <Upload className="mr-2 h-4 w-4" />
+                        <Donwload className="mr-2 h-4 w-4" />
                     )}
                     Confirmar Importação ({importSummary.totalValid} Itens)
                 </Button>
@@ -362,7 +362,7 @@ const MaterialConsumoExportImportDialog: React.FC<MaterialConsumoExportImportDia
                         {isProcessing ? (
                             <Loader2 className="mr-2 h-4 w-4 animate-spin" />
                         ) : (
-                            <Download className="mr-2 h-4 w-4" />
+                            <Upload className="mr-2 h-4 w-4" />
                         )}
                         Exportar ({diretrizes.length} Subitens)
                     </Button>
