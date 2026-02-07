e1f4eb8e2a4ebcab113d621a7418489f0045a557
[dyad] <ezratatsumi@hotmail.com>
2026-02-07 12:17:20 +0200

    [dyad] wrote 1 file(s)

--- src/components/pncp/ArpUasgSearch.tsx
+++ src/components/pncp/ArpUasgSearch.tsx
@@ -8,11 +8,19 @@ interface ArpUasgSearchProps {
     onItemPreSelect: (item: DetailedArpItem, pregaoFormatado: string, uasg: string) => void;
     // MUDANÇA: Array de IDs selecionados
     selectedItemIds: string[];
+    // NOVO: Função para limpar a seleção
+    onClearSelection: () => void;
 }
 
 // Este componente atua como um wrapper simples para o formulário
-const ArpUasgSearch: React.FC<ArpUasgSearchProps> = ({ onItemPreSelect, selectedItemIds }) => {
-    return <ArpUasgSearchForm onItemPreSelect={onItemPreSelect} selectedItemIds={selectedItemIds} />;
+const ArpUasgSearch: React.FC<ArpUasgSearchProps> = ({ onItemPreSelect, selectedItemIds, onClearSelection }) => {
+    return (
+        <ArpUasgSearchForm 
+            onItemPreSelect={onItemPreSelect} 
+            selectedItemIds={selectedItemIds} 
+            onClearSelection={onClearSelection} 
+        />
+    );
 };
 
 export default ArpUasgSearch;
