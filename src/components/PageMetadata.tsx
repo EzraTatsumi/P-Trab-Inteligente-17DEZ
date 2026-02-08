9eefaf09a5ed30222b2441818a56e3faabd7916b
[dyad] <ezratatsumi@hotmail.com>
2026-02-09 00:34:33 +0200

    [dyad] Fixar título da página do navegador

--- src/components/PageMetadata.tsx
+++ src/components/PageMetadata.tsx
@@ -8,26 +8,27 @@ interface PageMetadataProps {
 }
 
 const PageMetadata: React.FC<PageMetadataProps> = ({ title, description, canonicalPath }) => {
-  const fullTitle = `${title} | PTrab Inteligente`;
+  const browserTitle = "P Trab Inteligente | by TATSUMI";
+  const socialTitle = title ? `${title} | P Trab Inteligente` : browserTitle;
   const baseUrl = window.location.origin;
   const canonicalUrl = `${baseUrl}${canonicalPath}`;
 
   return (
     <Helmet>
-      <title>{fullTitle}</title>
+      <title>{browserTitle}</title>
       <meta name="description" content={description} />
       <link rel="canonical" href={canonicalUrl} />
       
       {/* Open Graph / Facebook */}
       <meta property="og:type" content="website" />
       <meta property="og:url" content={canonicalUrl} />
-      <meta property="og:title" content={fullTitle} />
+      <meta property="og:title" content={socialTitle} />
       <meta property="og:description" content={description} />
       
       {/* Twitter */}
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content={canonicalUrl} />
-      <meta property="twitter:title" content={fullTitle} />
+      <meta property="twitter:title" content={socialTitle} />
       <meta property="twitter:description" content={description} />
     </Helmet>
   );
