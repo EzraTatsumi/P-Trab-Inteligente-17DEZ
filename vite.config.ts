86ff69c51b94525b2e67e52592cd834f1c5d87f2
[dyad] <ezratatsumi@hotmail.com>
2026-02-14 00:12:09 +0200

    Add 1 file, update 1 file

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,8 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-
+import react from "@vitejs/plugin-react";
+import dyadComponentTagger from "@dyad-sh/react-vite-component-tagger";
+ 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+export default defineConfig({
+  plugins: [dyadComponentTagger(), react()],
+});
