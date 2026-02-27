2ffda07518f93e7b6be800411940776eecab9ef3
[dyad] <ezratatsumi@hotmail.com>
2026-02-27 11:31:09 +0200

    [dyad] Corrigindo erro de contexto do useNavigate - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -67,53 +67,51 @@ const AppContent = () => {
         status={status || null} 
       />
       <GhostModeBanner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/reset-password" element={<ResetPasswordPage />} />
-          
-          {/* Rotas Principais do P Trab */}
-          <Route path="/ptrab" element={<ProtectedRoute><PTrabManager /></ProtectedRoute>} />
-          <Route path="/ptrab/form" element={<ProtectedRoute><PTrabForm /></ProtectedRoute>} />
-          <Route path="/ptrab/print" element={<ProtectedRoute><PTrabReportManager /></ProtectedRoute>} />
-          <Route path="/ptrab/dor" element={<ProtectedRoute><DOREditor /></ProtectedRoute>} />
-          
-          {/* Rotas de Formulários Específicos (Logística) */}
-          <Route path="/ptrab/classe-i" element={<ProtectedRoute><ClasseIForm /></ProtectedRoute>} />
-          <Route path="/ptrab/classe-ii" element={<ProtectedRoute><ClasseIIForm /></ProtectedRoute>} />
-          <Route path="/ptrab/classe-iii" element={<ProtectedRoute><ClasseIIIForm /></ProtectedRoute>} />
-          <Route path="/ptrab/classe-v" element={<ProtectedRoute><ClasseVForm /></ProtectedRoute>} />
-          <Route path="/ptrab/classe-vi" element={<ProtectedRoute><ClasseVIForm /></ProtectedRoute>} />
-          <Route path="/ptrab/classe-vii" element={<ProtectedRoute><ClasseVIIForm /></ProtectedRoute>} />
-          <Route path="/ptrab/classe-viii" element={<ProtectedRoute><ClasseVIIIForm /></ProtectedRoute>} />
-          <Route path="/ptrab/classe-ix" element={<ProtectedRoute><ClasseIXForm /></ProtectedRoute>} />
-          
-          {/* Rotas de Formulários Específicos (Operacional) */}
-          <Route path="/ptrab/diaria" element={<ProtectedRoute><DiariaForm /></ProtectedRoute>} />
-          <Route path="/ptrab/verba-operacional" element={<ProtectedRoute><VerbaOperacionalForm /></ProtectedRoute>} />
-          <Route path="/ptrab/suprimento-fundos" element={<ProtectedRoute><SuprimentoFundosForm /></ProtectedRoute>} />
-          <Route path="/ptrab/passagem-aerea" element={<ProtectedRoute><PassagemForm /></ProtectedRoute>} />
-          <Route path="/ptrab/horas-voo-avex" element={<ProtectedRoute><HorasVooForm /></ProtectedRoute>} />
-          <Route path="/ptrab/concessionaria" element={<ProtectedRoute><ConcessionariaForm /></ProtectedRoute>} />
-          <Route path="/ptrab/material-consumo" element={<ProtectedRoute><MaterialConsumoForm /></ProtectedRoute>} />
-          <Route path="/ptrab/material-permanente" element={<ProtectedRoute><MaterialPermanenteForm /></ProtectedRoute>} />
-          <Route path="/ptrab/complemento-alimentacao" element={<ProtectedRoute><ComplementoAlimentacaoForm /></ProtectedRoute>} />
-          <Route path="/ptrab/servicos-terceiros" element={<ProtectedRoute><ServicosTerceirosForm /></ProtectedRoute>} />
-          
-          {/* Configurações */}
-          <Route path="/config/profile" element={<ProtectedRoute><UserProfilePage /></ProtectedRoute>} />
-          <Route path="/config/diretrizes" element={<ProtectedRoute><DiretrizesCusteioPage /></ProtectedRoute>} />
-          <Route path="/config/custos-operacionais" element={<ProtectedRoute><CustosOperacionaisPage /></ProtectedRoute>} />
-          <Route path="/config/visualizacao" element={<ProtectedRoute><VisualizacaoConfigPage /></ProtectedRoute>} />
-          <Route path="/config/om" element={<ProtectedRoute><OmConfigPage /></ProtectedRoute>} />
-          <Route path="/config/om/bulk-upload" element={<ProtectedRoute><OmBulkUploadPage /></ProtectedRoute>} />
-          <Route path="/config/ptrab-export-import" element={<ProtectedRoute><PTrabExportImportPage /></ProtectedRoute>} />
-          
-          <Route path="/share-ptrab" element={<SharePage />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/reset-password" element={<ResetPasswordPage />} />
+        
+        {/* Rotas Principais do P Trab */}
+        <Route path="/ptrab" element={<ProtectedRoute><PTrabManager /></ProtectedRoute>} />
+        <Route path="/ptrab/form" element={<ProtectedRoute><PTrabForm /></ProtectedRoute>} />
+        <Route path="/ptrab/print" element={<ProtectedRoute><PTrabReportManager /></ProtectedRoute>} />
+        <Route path="/ptrab/dor" element={<ProtectedRoute><DOREditor /></ProtectedRoute>} />
+        
+        {/* Rotas de Formulários Específicos (Logística) */}
+        <Route path="/ptrab/classe-i" element={<ProtectedRoute><ClasseIForm /></ProtectedRoute>} />
+        <Route path="/ptrab/classe-ii" element={<ProtectedRoute><ClasseIIForm /></ProtectedRoute>} />
+        <Route path="/ptrab/classe-iii" element={<ProtectedRoute><ClasseIIIForm /></ProtectedRoute>} />
+        <Route path="/ptrab/classe-v" element={<ProtectedRoute><ClasseVForm /></ProtectedRoute>} />
+        <Route path="/ptrab/classe-vi" element={<ProtectedRoute><ClasseVIForm /></ProtectedRoute>} />
+        <Route path="/ptrab/classe-vii" element={<ProtectedRoute><ClasseVIIForm /></ProtectedRoute>} />
+        <Route path="/ptrab/classe-viii" element={<ProtectedRoute><ClasseVIIIForm /></ProtectedRoute>} />
+        <Route path="/ptrab/classe-ix" element={<ProtectedRoute><ClasseIXForm /></ProtectedRoute>} />
+        
+        {/* Rotas de Formulários Específicos (Operacional) */}
+        <Route path="/ptrab/diaria" element={<ProtectedRoute><DiariaForm /></ProtectedRoute>} />
+        <Route path="/ptrab/verba-operacional" element={<ProtectedRoute><VerbaOperacionalForm /></ProtectedRoute>} />
+        <Route path="/ptrab/suprimento-fundos" element={<ProtectedRoute><SuprimentoFundosForm /></ProtectedRoute>} />
+        <Route path="/ptrab/passagem-aerea" element={<ProtectedRoute><PassagemForm /></ProtectedRoute>} />
+        <Route path="/ptrab/horas-voo-avex" element={<ProtectedRoute><HorasVooForm /></ProtectedRoute>} />
+        <Route path="/ptrab/concessionaria" element={<ProtectedRoute><ConcessionariaForm /></ProtectedRoute>} />
+        <Route path="/ptrab/material-consumo" element={<ProtectedRoute><MaterialConsumoForm /></ProtectedRoute>} />
+        <Route path="/ptrab/material-permanente" element={<ProtectedRoute><MaterialPermanenteForm /></ProtectedRoute>} />
+        <Route path="/ptrab/complemento-alimentacao" element={<ProtectedRoute><ComplementoAlimentacaoForm /></ProtectedRoute>} />
+        <Route path="/ptrab/servicos-terceiros" element={<ProtectedRoute><ServicosTerceirosForm /></ProtectedRoute>} />
+        
+        {/* Configurações */}
+        <Route path="/config/profile" element={<ProtectedRoute><UserProfilePage /></ProtectedRoute>} />
+        <Route path="/config/diretrizes" element={<ProtectedRoute><DiretrizesCusteioPage /></ProtectedRoute>} />
+        <Route path="/config/custos-operacionais" element={<ProtectedRoute><CustosOperacionaisPage /></ProtectedRoute>} />
+        <Route path="/config/visualizacao" element={<ProtectedRoute><VisualizacaoConfigPage /></ProtectedRoute>} />
+        <Route path="/config/om" element={<ProtectedRoute><OmConfigPage /></ProtectedRoute>} />
+        <Route path="/config/om/bulk-upload" element={<ProtectedRoute><OmBulkUploadPage /></ProtectedRoute>} />
+        <Route path="/config/ptrab-export-import" element={<ProtectedRoute><PTrabExportImportPage /></ProtectedRoute>} />
+        
+        <Route path="/share-ptrab" element={<SharePage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
     </>
   );
 };
@@ -122,12 +120,14 @@ const App = () => {
   return (
     <HelmetProvider>
       <QueryClientProvider client={queryClient}>
-        <SessionContextProvider>
-          <TooltipProvider>
-            <Toaster />
-            <AppContent />
-          </TooltipProvider>
-        </SessionContextProvider>
+        <BrowserRouter>
+          <SessionContextProvider>
+            <TooltipProvider>
+              <Toaster />
+              <AppContent />
+            </TooltipProvider>
+          </SessionContextProvider>
+        </BrowserRouter>
       </QueryClientProvider>
     </HelmetProvider>
   );
